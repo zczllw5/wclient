3a3b2b7840e90e392e61e1decc8fadde40edfc28
Lian <lian.wang.work@gmail.com>
2022-03-16 21:57:45 +0000

    early data implemented

--- earlyData.c
+++ earlyData.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
@@ -13,8 +14,74 @@
 #include "openssl/ssl.h"
 #include "openssl/err.h"
 
-#define MSG1_REQ "GET /index.html HTTP/1.1\r\nHOST: google.com\r\n\r\n"
+int res = 0;
 
+int err_exit(char *string)
+{
+    fprintf(stderr,"%s\n",string);
+    exit(0);
+}
+
+void get_hosts(char host[100][50]){
+    FILE *fp;
+    char *buff;
+    size_t fileStream;
+    long lSize;
+    
+    fp = fopen("top_1h.txt", "r");
+    
+    if (!fp)
+        exit(EXIT_FAILURE);
+    
+    // obtain file size:
+    fseek (fp, 0, SEEK_END); //Reposition stream position indicator
+    lSize = ftell (fp);      //Get current position in stream
+    rewind (fp);             //Set position of stream to the beginning
+
+    // // allocate memory to contain the whole file:
+    buff = (char*) malloc (sizeof(char)*lSize);
+    if (buff == NULL) {err_exit("Memory error");}
+
+    // copy the file into the buffer:
+    fileStream = fread (buff,1,lSize,fp);  //Read block of data from stream return size_t
+    if (fileStream != lSize) {
+        err_exit("Reading error");
+    }
+    char *piece = strtok(buff, " ");
+    strcpy(host[0], piece);
+    
+    for(int i = 1; i < 100; i++){
+        piece  = strtok(NULL, " ");
+        strcpy(host[i], piece);
+    }
+    
+    fclose(fp);
+}
+
+void hostname_to_ip(char hostname[100], char **ip)
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_in *h;
+    int rv;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // use AF_INET6 to force IPv6
+    hints.ai_socktype = SOCK_STREAM;
+
+    if((rv = getaddrinfo(hostname,"http",&hints,&servinfo)) != 0)
+    {
+        fprintf(stderr,"getaddrinfo: %s\n",gai_strerror(rv));
+    }
+
+    for(p = servinfo; p != NULL;p = p->ai_next)
+    {
+        h = (struct sockaddr_in*)p->ai_addr;
+        *ip = inet_ntoa(h->sin_addr);
+    }
+    
+    freeaddrinfo(servinfo);
+}
 void get_error()
 {
     unsigned long error;
@@ -81,18 +148,34 @@ SSL_CTX *create_context()
 
     printf("SSL context created\n");
 
+    int err;
+    int minVersion = TLS1_3_VERSION;
+    int maxVersion = TLS1_3_VERSION;
+
+    err = SSL_CTX_set_min_proto_version(ctx,minVersion);
+    if(err==0)
+        err_exit("set min version error\n");
+    else if(err==1)
+        printf("min version is: %ld\n",SSL_CTX_get_min_proto_version(ctx));
+    
+    err = SSL_CTX_set_max_proto_version(ctx,maxVersion);
+    if(err==0)
+        err_exit("SSL_CTX_set_max_proto_version error");
+    else if(err==1)
+        printf("max version is: %ld\n",SSL_CTX_get_max_proto_version(ctx));
+
     return ctx;
 err_handler:
     SSL_CTX_free(ctx);
     return NULL;
 }
 
-SSL *create_ssl_object(SSL_CTX *ctx)
+SSL *create_ssl_object(SSL_CTX *ctx, char * ip)
 {
     SSL *ssl;
     int fd;
 
-    fd = do_tcp_connection("142.250.200.46", 443);
+    fd = do_tcp_connection(ip, 443);
     if (fd < 0) {
         printf("TCP connection establishment failed\n");
         return NULL;
@@ -138,10 +221,10 @@ void ssl_error_exit(SSL *ssl, int ret)
             goto end;
         case SSL_ERROR_SYSCALL:
             fprintf(stderr, "errno = %s\n", strerror(errno));
-            //printf("non-recoverable, fatal I/O error occurred\n");
             goto end;
         case SSL_ERROR_SSL:
             printf("non-recoverable, fatal error in the SSL library occurred, usually a protocol error.\n");
+            ERR_peek_error();
             goto end;
   }
   end:
@@ -149,9 +232,8 @@ void ssl_error_exit(SSL *ssl, int ret)
     exit(0);
 }
 
-int do_early_data_transfer(SSL *ssl)
+int do_early_data_transfer(SSL *ssl, char* MSG1_REQ)
 {
-    char *msg_req = "Hello, I am client early data!";
     char buf[100000] = {0};
     size_t written;
     int ret;
@@ -160,22 +242,25 @@ int do_early_data_transfer(SSL *ssl)
     //ssl_error_exit(ssl,SSL_write_early_data(ssl, msg_req, strlen(msg_req), &written));
     if (ret <= 0) {
     	printf("SSL_write_early_data failed ret=%d\n", ret);
-	return -1;
-    }
-    printf("Early data write sucessed\n");
+	    //return -1;
+    } else {
+        printf("Early data write sucessed\n");
 
-    ret = SSL_read(ssl, buf, sizeof(buf) - 1);
-    //ssl_error_exit(ssl,SSL_read(ssl, buf, sizeof(buf) - 1));
-    if (ret <= 0) {
-        printf("SSL_read read early data failed ret=%d\n", ret);
-	return -1;
+        ret = SSL_read(ssl, buf, sizeof(buf) - 1);
+        //ssl_error_exit(ssl,SSL_read(ssl, buf, sizeof(buf) - 1));
+        if (ret <= 0) {
+            printf("SSL_read read early data failed ret=%d\n", ret);
+        return -1;
+        }
+        printf("Early data read '%s'\n", buf);
+        res++;
     }
-    printf("Early data read '%s'\n", buf);
+    
 
     return 0;
 }
 
-int do_data_transfer(SSL *ssl)
+int do_data_transfer(SSL *ssl, char *MSG1_REQ)
 {
     char buf[10000] = {0};
     int ret, i;
@@ -197,7 +282,7 @@ int do_data_transfer(SSL *ssl)
     return 0;
 }
 
-int tls13_client(int con_count)
+int tls13_client(char *ip, char *req)
 {
     SSL_CTX *ctx;
     SSL *ssl = NULL;
@@ -207,7 +292,7 @@ int tls13_client(int con_count)
     int ret;
     int i;
 
-    for (i = 0; i < con_count; i++) {
+    for (i = 0; i < 2; i++) {
         ctx = create_context();
         if (!ctx) {
 	        return -1;
@@ -221,7 +306,7 @@ int tls13_client(int con_count)
             }
         }
 
-        ssl = create_ssl_object(ctx);
+        ssl = create_ssl_object(ctx, ip);
         if (!ssl) {
 	        goto err_handler;
         }
@@ -235,14 +320,15 @@ int tls13_client(int con_count)
         }
 
         if (i >= 1) {
-            if (do_early_data_transfer(ssl)) {
+            if (do_early_data_transfer(ssl, req)) {
                 printf("Early data transfer over TLS failed\n");
                 goto err_handler;
             }
-            printf("Early data transfer over TLS suceeded\n");
+            //printf("Early data transfer over TLS suceeded\n");
         }
 
         ret = SSL_connect(ssl);
+        //ssl_error_exit(ssl,SSL_connect(ssl));
         if (ret != 1) {
             printf("SSL connect failed%d\n", ret);
 	        get_error();
@@ -251,7 +337,7 @@ int tls13_client(int con_count)
         printf("SSL connect succeeded\n");
 
         if(i < 1){
-            if (do_data_transfer(ssl)) {
+            if (do_data_transfer(ssl, req)) {
                 printf("Data transfer over TLS failed\n");
                 goto err_handler;
             }
@@ -283,12 +369,33 @@ err_handler:
     return ret_val;
 }
 
+int iteration(){
+    char *ip = (char *)malloc(sizeof(char)*50);
+    char host[100][50];
+    char MSG1_REQ[1024];
+
+    get_hosts(host); 
+
+    for(int i = 0; i < 100; i++){
+
+        hostname_to_ip(host[i], &ip);
+        printf("%i. %s resolved to %s \n", i, host[i], ip);
+
+        printf("OpenSSL version: %s, %s\n", OpenSSL_version(OPENSSL_VERSION), OpenSSL_version(OPENSSL_BUILT_ON));
+
+        snprintf(MSG1_REQ, sizeof(MSG1_REQ), "GET /main.html HTTP/1.1\r\nHOST: %s\r\n\r\n", host[i]);
+        if (tls13_client(ip, MSG1_REQ)) {
+            printf("TLS13 client connection failed\n");
+            continue;
+        }
+        printf("********************************************************************************************************************************\n");
+    }
+    printf("There are %i early data transport succeeded!\n", res);
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    printf("OpenSSL version: %s, %s\n", OpenSSL_version(OPENSSL_VERSION), OpenSSL_version(OPENSSL_BUILT_ON));
-    if (tls13_client(2)) {
-        printf("TLS13 client connection failed\n");
-        return -1;
-    }
+    iteration();
     return 0;
 }
